05f73778ccf09781c0e6b89b522530c9c4bfce85
Erik2K <erikgallardo2000@gmail.com>
2023-08-12 02:26:05 +0200

    upodated configuration

---
+++ colors.py
@@ -0,0 +1,57 @@
+gruvbox = {
+    'bg':           '#282828',
+    'bg0_h':        '#1d2021',
+    'bg0_s':        '#32302f',
+    'bg0':          '#282828',
+    'bg1':          '#3c3836',
+    'bg2':          '#504945',
+    'bg3':          '#665c54',
+    'bg4':          '#7c6f64',
+    
+    'fg':           '#ebdbb2',
+    'fg0':          '#fbf1c7',
+    'fg1':          '#ebdbb2',
+    'fg2':          '#d5c4a1',
+    'fg3':          '#bdae93',
+    'bg4':          '#a89984',
+    
+    'dark-red':     '#cc241d',
+    'red':          '#fb4934',
+    
+    'dark-green':   '#98971a',
+    'green':        '#b8bb26',
+    
+    'dark-yellow':  '#d79921',
+    'yellow':       '#fabd2f',
+    
+    'dark-blue':    '#458588',
+    'blue':         '#83a598',
+    
+    'dark-purple':  '#b16286',
+    'purple':       '#d3869b',
+    
+    'dark-aqua':    '#689d6a',
+    'aqua':         '#8ec07c',
+    
+    'dark-gray':    '#928374',
+    'gray':         '#a89984',
+    
+    'dark-orange':  '#d65d0e',
+    'orange':       '#fe8019',
+}
+
+nord_fox = {
+    'bg': "#2e3440",
+    'fg': "#b9bfca",
+    'fg_gutter': "#4b5668",
+    'black': "#3b4252",
+    'red': "#bf616a",
+    'green': "#a3be8c",
+    'yellow': "#ebcb8b",
+    'blue': "#81a1c1",
+    'magenta': "#b48ead",
+    'cyan': "#88c0d0",
+    'white': "#e5e9f0",
+    'orange': "#c9826b",
+    'pink': "#bf88bc",
+}
--- config.py
+++ config.py
@@ -1,41 +1,36 @@
-# Copyright (c) 2010 Aldo Cortesi
-# Copyright (c) 2010, 2014 dequis
-# Copyright (c) 2012 Randall Ma
-# Copyright (c) 2012-2014 Tycho Andersen
-# Copyright (c) 2012 Craig Barnes
-# Copyright (c) 2013 horsik
-# Copyright (c) 2013 Tao Sauvage
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-from typing import List
-
 import os
 import subprocess
 
 from libqtile import bar, layout, widget, hook, extension
+from libqtile.layout.columns import Columns
+from libqtile.layout.stack import Stack
+from libqtile.layout.floating import Floating
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
+from libqtile.config import (
+    Click,
+    Drag,
+    DropDown,
+    Group,
+    Key,
+    Match,
+    ScratchPad,
+    Screen
+)
+
+from colors import gruvbox
+from bar2 import bar
 
 mod = "mod4"
 terminal = guess_terminal()
+
+# ██╗  ██╗███████╗██╗   ██╗██████╗ ██╗███╗   ██╗██████╗ ███████╗
+# ██║ ██╔╝██╔════╝╚██╗ ██╔╝██╔══██╗██║████╗  ██║██╔══██╗██╔════╝
+# █████╔╝ █████╗   ╚████╔╝ ██████╔╝██║██╔██╗ ██║██║  ██║███████╗
+# ██╔═██╗ ██╔══╝    ╚██╔╝  ██╔══██╗██║██║╚██╗██║██║  ██║╚════██║
+# ██║  ██╗███████╗   ██║   ██████╔╝██║██║ ╚████║██████╔╝███████║
+# ╚═╝  ╚═╝╚══════╝   ╚═╝   ╚═════╝ ╚═╝╚═╝  ╚═══╝╚═════╝ ╚══════╝
 
 keys = [
     Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
@@ -64,126 +59,136 @@ keys = [
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    
+    # Toggle floating and fullscreen
+    Key([mod], "f", lazy.window.toggle_fullscreen(),
+        desc="Toggle fullscreen mode"),
+    Key([mod, "shift"], "space", lazy.window.toggle_floating(),
+        desc="Toggle fullscreen mode"),
 
     # Dmenu
     Key([mod], "p", lazy.run_extension(extension.DmenuRun(
-        font="Andika-8",
-        fontsize="13",
+        font="JetBrainsMono Nerd Font",
+        fontsize="12",
         dmenu_command="dmenu_run",
         dmenu_prompt=">_",
         dmenu_height=10,
         dmenu_lines=15,
-        background="#15181a",
-        foreground="#00ff00",
-        selected_background="#079822",
-        selected_foreground="#fff",
+        background=gruvbox['bg'],
+        foreground=gruvbox['gray'],
+        selected_background=gruvbox['bg1'],
+        selected_foreground=gruvbox['blue'],
 ))),
 ]
 
-groups = [Group(i) for i in "123456789"]
+#  ██████╗ ██████╗  ██████╗ ██╗   ██╗██████╗ ███████╗
+# ██╔════╝ ██╔══██╗██╔═══██╗██║   ██║██╔══██╗██╔════╝
+# ██║  ███╗██████╔╝██║   ██║██║   ██║██████╔╝███████╗
+# ██║   ██║██╔══██╗██║   ██║██║   ██║██╔═══╝ ╚════██║
+# ╚██████╔╝██║  ██║╚██████╔╝╚██████╔╝██║     ███████║
+#  ╚═════╝ ╚═╝  ╚═╝ ╚═════╝  ╚═════╝ ╚═╝     ╚══════╝                                            
+
+groups = [
+    Group(
+        '1',
+        label="一",
+        matches=[
+            Match(wm_class='firefox'),
+            Match(wm_class='brave'),
+            Match(wm_class='qutebrowser')
+        ],
+        layout="stack"
+    ),
+    Group('2', label="二", layout="monadtall"),
+    Group('3', label="三", layout="columns"),
+    Group(
+        '4',
+        label="四",
+        matches=[
+            Match(wm_class="whatsdesk")
+        ],
+        layout="stack"
+    ),
+    Group(
+        '5',
+        label="五",
+        layout="stack"
+    ),
+    Group('6', label="六", matches=[
+          Match(wm_class="thunderbird")], layout="monadtall"),
+    Group('7', label="七", layout="monadtall"),
+    Group('8', label="八", layout="monadtall"),
+    Group('9', label="九", layout="monadtall"),
+]
+
 
 for i in groups:
-    keys.extend(
+    keys.extend([
+        # mod1 + letter of group = switch to group
+        Key([mod], i.name, lazy.group[i.name].toscreen(),
+            desc="Switch to group {}".format(i.name)),
+
+        Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+            desc="move focused window to group {}".format(i.name)),
+    ])
+                    
+groups.append(
+    ScratchPad(
+        'scratchpad',
         [
-            # mod1 + letter of group = switch to group
-            Key(
-                [mod],
-                i.name,
-            lazy.group[i.name].toscreen(),
-                desc="Switch to group {}".format(i.name),
+            DropDown(
+                'mixer',
+                'pavucontrol',
+                width=0.4,
+                height=0.6,
+                x=0.3,
+                y=0.1,
+                opacity=1
             ),
-            # mod1 + shift + letter of group = switch to & move focused window to group
-            Key(
-                [mod, "shift"],
-                i.name,
-                lazy.window.togroup(i.name, switch_group=True),
-                desc="Switch to & move focused window to group {}".format(i.name),
-            ),
-            # Or, use below if you prefer not to switch to that group.
-            # # mod1 + shift + letter of group = move focused window to group
-            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-            #     desc="move focused window to group {}".format(i.name)),
         ]
     )
+)
+
+keys.extend([
+    Key(["control"], "1", lazy.group['scratchpad'].dropdown_toggle('term')),
+    Key(["control"], "2", lazy.group['scratchpad'].dropdown_toggle('mixer')),
+    Key(["control"], "3", lazy.group['scratchpad'].dropdown_toggle('pomodoro')),
+    Key(["control"], "4", lazy.group['scratchpad'].dropdown_toggle('blueman')),
+])
+        
+# ██╗      █████╗ ██╗   ██╗ ██████╗ ██╗   ██╗████████╗███████╗
+# ██║     ██╔══██╗╚██╗ ██╔╝██╔═══██╗██║   ██║╚══██╔══╝██╔════╝
+# ██║     ███████║ ╚████╔╝ ██║   ██║██║   ██║   ██║   ███████╗
+# ██║     ██╔══██║  ╚██╔╝  ██║   ██║██║   ██║   ██║   ╚════██║
+# ███████╗██║  ██║   ██║   ╚██████╔╝╚██████╔╝   ██║   ███████║
+# ╚══════╝╚═╝  ╚═╝   ╚═╝    ╚═════╝  ╚═════╝    ╚═╝   ╚══════╝
 
 layouts = [
-    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
-    layout.Max(),
-    # Try more layouts by unleashing below layouts.
-    # layout.Stack(num_stacks=2),
-    # layout.Bsp(),
-    # layout.Matrix(),
-    # layout.MonadTall(),
-    # layout.MonadWide(),
-    # layout.RatioTile(),
-    # layout.Tile(),
-    # layout.TreeTab(),
-    # layout.VerticalTile(),
-    # layout.Zoomy(),
-]
-
-widget_defaults = dict(
-    font="sans",
-    fontsize=12,
-    padding=3,
-)
-extension_defaults = widget_defaults.copy()
-
-screens = [
-    Screen(
-        top=bar.Bar(
-            [
-                widget.CurrentLayout(),
-                widget.GroupBox(),
-                widget.Prompt(),
-                widget.WindowName(),
-                widget.Chord(
-                    chords_colors={
-                        "launch": ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
-                ),
-                widget.TextBox("default config", name="default"),
-                widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
-                # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
-                # widget.StatusNotifier(),
-                widget.Systray(),
-                widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
-                widget.QuickExit(),
-            ],
-            24,
-            background='#222222',
-            opacity=1
-        ),
-        wallpaper='~/Documents/wallpapers/red-samurai-wallpaper.png',
-        wallpaper_mode='stretch',
+    Columns(
+        border_normal=gruvbox['bg1'],
+        border_focus=gruvbox['red'],
+        border_width=2,
+        border_normal_stack=gruvbox['bg1'],
+        border_focus_stack=gruvbox['red'],
+        border_on_single=2,
+        margin=8,
+        margin_on_single=10,
     ),
-    Screen(
-        wallpaper='~/Documents/wallpapers/red-samurai-wallpaper.png',
-        wallpaper_mode='stretch',
-    ),
-    Screen(
-        wallpaper='~/Documents/wallpapers/red-samurai-wallpaper.png',
-        wallpaper_mode='stretch',
+    Stack(
+        border_normal=gruvbox['bg1'],
+        border_focus=gruvbox['red'],
+        border_width=2,
+        num_stacks=1,
+        margin=10,
     ),
 ]
 
-# Drag floating layouts.
-mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
-    Click([mod], "Button2", lazy.window.bring_to_front()),
-]
-
-dgroups_key_binder = None
-dgroups_app_rules = []  # type: list
-follow_mouse_focus = True
-bring_front_click = False
-cursor_warp = False
-floating_layout = layout.Floating(
+floating_layout = Floating(
+    border_normal=gruvbox['bg1'],
+    border_focus=gruvbox['red'],
+    border_width=2,
     float_rules=[
-        # Run the utility of `xprop` to see the wm class and name of an X client.
-        *layout.Floating.default_float_rules,
+        *Floating.default_float_rules,
         Match(wm_class="confirmreset"),  # gitk
         Match(wm_class="makebranch"),  # gitk
         Match(wm_class="maketag"),  # gitk
@@ -192,26 +197,70 @@ floating_layout = layout.Floating(
         Match(title="pinentry"),  # GPG key password entry
     ]
 )
+
+mouse = [
+    Drag(
+        [mod], 
+        "Button1", 
+        lazy.window.set_position_floating(), 
+        start=lazy.window.get_position()
+    ),
+    Drag(
+        [mod], 
+        "Button3", 
+        lazy.window.set_size_floating(), 
+        start=lazy.window.get_size()
+    ),
+    Click(
+        [mod], 
+        "Button2", 
+        lazy.window.bring_to_front()
+    ),
+]
+
+widget_defaults = dict(
+    font='JetBrainsMono Nerd Font',
+    fontsize=12,
+    padding=10,
+    background=gruvbox['bg'],
+)
+
+extension_defaults = widget_defaults.copy()
+
+screens = [
+    Screen(
+        top=bar,
+        wallpaper='~/Documents/wallpapers/red-samurai-wallpaper.png',
+        wallpaper_mode='stretch',
+    ),
+    Screen(
+        wallpaper='~/Documents/wallpapers/red-samurai-wallpaper.png',
+        wallpaper_mode='stretch',
+    ),
+    Screen(
+        wallpaper='~/Documents/wallpapers/red-samurai-wallpaper.png',
+        wallpaper_mode='stretch',
+    ),
+]
+
+dgroups_key_binder = None
+dgroups_app_rules = []
+follow_mouse_focus = False
+bring_front_click = ''
+cursor_warp = False
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
-
-# If things like steam games want to auto-minimize themselves when losing
-# focus, should we respect this or not?
 auto_minimize = True
-
-# When using the Wayland backend, this can be used to configure input devices.
-wl_input_rules = None
-
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
-# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
-# java that happens to be on java's whitelist.
 wmname = "LG3D"
+
+
+# ██╗  ██╗ ██████╗  ██████╗ ██╗  ██╗███████╗
+# ██║  ██║██╔═══██╗██╔═══██╗██║ ██╔╝██╔════╝
+# ███████║██║   ██║██║   ██║█████╔╝ ███████╗
+# ██╔══██║██║   ██║██║   ██║██╔═██╗ ╚════██║
+# ██║  ██║╚██████╔╝╚██████╔╝██║  ██╗███████║
+# ╚═╝  ╚═╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝
 
 # Runs the autostart script
 @hook.subscribe.startup_once
